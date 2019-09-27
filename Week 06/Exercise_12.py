8e84dc4b4de51bf5584a74b027e2ef19903ff9e6
thevexis <thevexis@users.noreply.github.com>
2019-09-27 13:18:47 -0400

    Update Exercise_12.py

--- Week 06/Exercise_12.py
+++ Week 06/Exercise_12.py
@@ -14,9 +14,11 @@ import math as math
 import numpy as np
 import numpy.random as rand
 
-N=1000
+y = []
+x = []
+N = 100
 summation = []
-b = 1
+b = 1   
 a = 0
 
 for i in range(N): 
@@ -25,12 +27,43 @@ for i in range(N):
         f = sum(x)
         function = f**2
     summation.append(function)
-    
+
+
 summation = sum(summation)/N
     
-print(summation)
+print("10D Integral approximation (with N = 100):", summation)
 
 
+error = []
+x_values = []
+N = 0
+summation = []
+b = 1
+a = 0
 
+for l in range(100):
+    N += 1
+    for i in range(N): 
+        for l in range(10):
+            x = rand.rand(10)*(b-a)
+            f = sum(x)
+            function = f**2
+        summation.append(function)
+    error.append((25.83333 - sum(summation)/N)*100)
+    x_values.append(1/(N**(0.5)))
+    
+    
+    
+    
+plt.plot(x_values,error,'o')
+plt.show()
 
+"""
+Bigger N gets the smaller the error. It seems that on the graph at the values of
+about 0.2 is when the value starts to become accurate. This 0.2 value is equivalent to
+N = 400. This computation instead of taking 10^6 years with the brute force method
+takes a few seconds with the MC method. It is more efficient to use the MC method when 
+the dimensions become greater than 2.
+
+"""
 
