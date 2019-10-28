9dad4af8578191c6ecae97e781c4a5d63aa48164
thevexis <thevexis@users.noreply.github.com>
2019-10-28 09:57:36 -0400

    Create Lab_05.py

---
+++ Labs/Lab_05.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 28 09:54:29 2019
+
+@author: thevexis
+Lab_05
+Ordinary Differential Equations - Non-linear Oscillators 
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
