1ca79734165d278e331727519c6b0fb30ce9def5
thevexis <thevexis@users.noreply.github.com>
2019-11-01 18:04:55 -0400

    Update Lab_05.py

--- Labs/Lab_05.py
+++ Labs/Lab_05.py
@@ -13,26 +13,19 @@ import math as math
 import numpy as np 
 
 
-def EulerFriction(mass,p,k,x0,v0,friction=True,ExternalForce=True):
+def EulerFriction(mass,p,k,x0,v0,friction=False,ExternalForce=False):
     
     g = 9.81 #m/s^2
-    dt = 0.1
+    dt = 0.001
     x_array = []
     v_array = []
     t_array = np.arange(0,100,dt)
-        
     
-    if ExternalForce == True:
-        F0 = 500
-        Fext = F0*np.sin(*t_array)
-    else:
-        Fext = 0
+    if (friction == True and ExternalForce==False):
         
-        
-    if friction == True:
         mu_s = 1.0e-2
         mu_k = 1.0e-2
-        
+     
         if abs(v0) < 1.0e-5:
             static  = -mu_s * mass * g 
             kinetic = 0.0  
@@ -40,9 +33,9 @@ def EulerFriction(mass,p,k,x0,v0,friction=True,ExternalForce=True):
             static  = 0.0
             kinetic = -mu_k * mass * g * (v0 / abs(v0))
         
-        v = v0 + (dt/mass)*((-k*(x0**(p-1))) + static + kinetic + Fext )
+        v = v0 + (dt/mass)*((-k*(x0**(p-1))) + static + kinetic)
         x = x0 + dt*v
-    
+        
         for t in t_array:
             
             if abs(v) < 1.0e-5:
@@ -52,7 +45,7 @@ def EulerFriction(mass,p,k,x0,v0,friction=True,ExternalForce=True):
                 static  = 0.0
                 kinetic = -mu_k * mass * g * (v / abs(v))
             
-            v = v + (dt/mass)*((-k*(x**(p-1))) + static + kinetic + Fext )
+            v = v + (dt/mass)*((-k*(x**(p-1))) + static + kinetic)
             x = x + dt*v
             
             
@@ -61,93 +54,115 @@ def EulerFriction(mass,p,k,x0,v0,friction=True,ExternalForce=True):
             
         return x_array, v_array, t_array
             
-    else:
+    elif (ExternalForce == True and friction==False):
+    
+        w = (k/mass)**0.5
         
-        if ExternalForce == True:
-            for k in range(0,len(v_array)):
-                if v_array[k] < 1.0e-4:
-                    F0 = 500
-                    Fext = F0*np.sin(t_array[k])
-                else:
-                    Fext = 0
-            x_array = []
-            v_array = []
-            t_array = np.arange(0,100,dt)
+        F0 = 10
+        t_initial = 0.0
+        Fext = F0*np.sin(w*t_initial)
     
-             v = v0 + (dt/mass)*(-k*(x0**(p-1)))
-             x = x0 + dt*v
-    
-             for t in t_array:
+        v = v0 + (dt/mass)*(-k*(x0**(p-1)) + Fext)
+        x = x0 + dt*v
+        
+        for t in t_array:
+        
+            Fext = F0*np.sin(w*t)
             
-                 v = v + (dt/mass)*(-k*(x**(p-1)) + Fext)
-                 x = x + dt*v
+            v = v + (dt/mass)*((-k*(x**(p-1))) + Fext)
+            x = x + dt*v
                  
-                 x_array.append(x)
-                 v_array.append(v)
+            x_array.append(x)
+            v_array.append(v)
     
-             return x_array, v_array, t_array
-         
+        return x_array, v_array, t_array
+                        
+    elif (friction == True and ExternalForce == True):
+        
+        w = (k/mass)**0.5
+        mu_s = 1.0e-1
+        mu_k = 1.0e-1
+        F0 = 10
+        t_initial = 0.0
+        Fext = F0*np.sin(w*t_initial)
+        
+        if abs(v0) < 1.0e-5:
+            static  = -mu_s * mass * g 
+            kinetic = 0.0  
         else:
-            Fext = 0
+            static  = 0.0
+            kinetic = -mu_k * mass * g * (v0 / abs(v0))
+
+        v = v0 + (dt/mass)*((-k*(x0**(p-1))) + Fext + static + kinetic)
+        x = x0 + dt*v
+        
+        for t in t_array:
             
-         static = 0.0
-         kinetic = 0.0
+            Fext = F0*np.sin(w*t)
+            
+            if abs(v) < 1.0e-5:
+                static  = -mu_s * mass * g 
+                kinetic = 0.0  
+            else:
+                static  = 0.0
+                kinetic = -mu_k * mass * g * (v / abs(v))
+            
+            v = v + (dt/mass)*((-k*(x**(p-1))) + Fext + static + kinetic)
+            x = x + dt*v
+                 
+            x_array.append(x)
+            v_array.append(v)
+    
+        return x_array, v_array, t_array
          
-         x_array = []
-         v_array = []
-         t_array = np.arange(0,100,dt)
+    else:
+
+        v = v0 + (dt/mass)*(-k*(x0**(p-1)))
+        x = x0 + dt*v
     
-         v = v0 + (dt/mass)*(-k*(x0**(p-1)))
-         x = x0 + dt*v
-    
-         for t in t_array:
+        for t in t_array:
             
-            v = v + (dt/mass)*(-k*(x**(p-1)) + Fext)
+            v = v + (dt/mass)*(-k*(x**(p-1)))
             x = x + dt*v
         
             x_array.append(x)
             v_array.append(v)
     
-         return x_array, v_array, t_array
+        return x_array, v_array, t_array
+    
 
-    
-    
-    x_array = []
-    v_array = []
-    t_array = np.arange(0,100,dt)
-    
-    v = v0 + (dt/m)*(-k*(x0**(p-1)))
-    x = x0 + dt*v
-    
-    for t in t_array:
-        
-        v = v + (dt/m)*(-k*(x**(p-1)) + Fext)
-        x = x + dt*v
-        
-        x_array.append(x)
-        v_array.append(v)
-    
-    return x_array, v_array, t_array
-
-
+x, v, t = EulerFriction(5,2,10,1.0,0.0,friction=False,ExternalForce=False)
+plt.plot(t,x,label='Position')
+plt.plot(t,v,'r',label='Velocity')
+plt.xlim(0,60)
+plt.title('No External Force and  No Friction')
+plt.legend()
+plt.show()
 
 x, v, t = EulerFriction(5,2,10,1.0,0.0,friction=True,ExternalForce=False)
 plt.plot(t,x,label='Position')
 plt.xlim(0,60)
 plt.plot(t,v,'r',label='Velocity')
+plt.title('Friction')
 plt.legend()
 plt.show()
 
-"""
+
 x, v, t = EulerFriction(5,2,10,1.0,0.0,friction=False,ExternalForce=True)
-plt.plot(t,x)
+plt.plot(t,x,label='Position')
+plt.plot(t,v,'r',label='Velocity')
 plt.xlim(0,60)
+plt.title('External Force')
+plt.legend()
 plt.show()
 
-plt.plot(t,v,'r')
+x, v, t = EulerFriction(5,2,10,1.0,0.0,friction=True,ExternalForce=True)
+plt.plot(t,x,label='Position')
+plt.plot(t,v,'r',label='Velocity')
 plt.xlim(0,60)
+plt.title('External Force and Friction')
+plt.legend()
 plt.show()
-"""
 
 
 
