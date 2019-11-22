31db5ceed226d6b13d5d8f89d0a160bd51f13067
thevexis <thevexis@users.noreply.github.com>
2019-11-22 15:12:59 -0500

    Update Capacitor.py

--- Project/Capacitor.py
+++ Project/Capacitor.py
@@ -20,19 +20,32 @@ Xboxmax = 200
 Yboxmax = 200
 Plate1 = 100 #volts
 Plate2 = -100
-Potential = np.zeros((Xboxmax,Yboxmax),float)
 plate_width = 100
-plate_initial = 50 #where one side of the plate starts in the x range
-plate_final = plate_initial + plate_width #final position of the plate in x range
-tolerance = 100
-rho = 5*np.ones((Xboxmax,Yboxmax),float) #charge density
-delta = 0.01
+platey0 = 50 # y initial position of the plate
+platey1 = platey0 + plate_width # y final position of plate
+tolerance = 1000
+delta = 0.1
+thickness = 20
+platex0 = 75 # x position of the first plate
+platex1 = 125 # x position of the second plate
+charge_density = 1 #density of charges
+
+Potential = np.zeros((Yboxmax,Xboxmax),float)
+
+rho = np.zeros((Xboxmax,Yboxmax),float) #charge density
+
+for y in range(0,Yboxmax):
+    for x in range(0,Xboxmax):
+        if y >= platey0 and y <= platey1:
+            if x >= platex0 and x <= platex1:
+                rho[x,y] = charge_density
 
 
-for k in range(plate_initial,plate_final):
-    Potential[k,75] = Plate1
-for l in range(plate_initial,plate_final):
-    Potential[l,125] = Plate2
+for k in range(platey0,platey1):
+    Potential[k,platex0] = Plate1
+
+for l in range(platey0,platey1):
+    Potential[l,platex1] = Plate2
 
 
 for tol in range(tolerance):
@@ -46,6 +59,15 @@ y = range(0,Yboxmax,1)
 X, Y = np.meshgrid(x,y)
 
 Z = Potential
+p = rho
+
+fig = plt.figure()
+bx = Axes3D(fig)
+bx.plot_surface(X,Y,p, cmap=cm.ocean)
+bx.set_xlabel('X')
+bx.set_ylabel('Y')
+bx.set_zlabel('Charge Density')
+plt.show()
 
 fig = plt.figure()
 ax = Axes3D(fig)
