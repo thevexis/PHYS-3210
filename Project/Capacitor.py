7bae16fb153a11cedeb83e9e1f9c57f1c80c9b14
thevexis <thevexis@users.noreply.github.com>
2019-11-22 14:06:10 -0500

    Update Capacitor.py

--- Project/Capacitor.py
+++ Project/Capacitor.py
@@ -20,41 +20,47 @@ Xboxmax = 200
 Yboxmax = 200
 Plate1 = 100 #volts
 Plate2 = -100
-plate_width = 100
-platey0 = 50 # y initial position of the plate
-platey1 = platey0 + plate_width # y final position of plate
-tolerance = 1000
+plate_width = 1000
+tolerance = 100
 delta = 0.1
 thickness = 20
-platex0 = 75 # x position of the first plate
-platex1 = 125 # x position of the second plate
+platey0 = 500 # y initial position of the plate
+platey1 = platey0 + plate_width  # y final position of plate
+platex0 = 750 # x position of the first plate
+platex1 = 800 # x position of the second plate
 charge_density = 1 #density of charges
 
-Potential = np.zeros((Yboxmax,Xboxmax),float)
+x = np.arange(0,Xboxmax,delta)
+y = np.arange(0,Yboxmax,delta)
 
-rho = np.zeros((Xboxmax,Yboxmax),float) #charge density
+lenx = len(x)
+leny = len(y)
 
-for y in range(0,Yboxmax):
-    for x in range(0,Xboxmax):
-        if y >= platey0 and y <= platey1:
-            if x >= platex0 and x <= platex1:
+Potential = np.zeros((lenx,leny),float)
+
+
+rho = np.zeros((lenx,leny),float) #charge density
+for y in range(0,leny):
+    for x in range(0,lenx):
+        if y >= (platey0*(delta**-1)) and y <= (platey1*(delta**-1)):
+            if x >= (platex0*(delta**-1)) and x <= (platex1*(delta**-1)):
                 rho[x,y] = charge_density
 
 
 for k in range(platey0,platey1):
     Potential[k,platex0] = Plate1
 
-for l in range(platey0,platey1):
+for l in range(platey0-1,platey1-1):
     Potential[l,platex1] = Plate2
 
 
 for tol in range(tolerance):
-    for i in range(1,Xboxmax-2):
-        for j in range(1,Yboxmax-2):
+    for i in range(1,lenx-2):
+        for j in range(1,leny-2):
             Potential[i,j] = 0.25*(Potential[i+1,j] + Potential[i-1,j] + Potential[i,j+1] + Potential[i,j-1]) + np.pi*(rho[i,j])*(delta**2)        
 
-x = range(0,Xboxmax,1)
-y = range(0,Yboxmax,1)
+x = np.arange(0,Xboxmax,delta)
+y = np.arange(0,Yboxmax,delta)
 
 X, Y = np.meshgrid(x,y)
 
