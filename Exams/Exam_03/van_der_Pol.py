bcbb05d37e2fe6dfff3403ee0e016f6c0278f33c
thevexis <thevexis@users.noreply.github.com>
2019-11-29 14:59:27 -0500

    update

---
+++ Exams/Exam_03/van_der_Pol.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 29 14:57:02 2019
+
+@author: thevexis
+
+Exam_03
+van der Pol’s Equation & the Electric Field of a Laser
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+
+
